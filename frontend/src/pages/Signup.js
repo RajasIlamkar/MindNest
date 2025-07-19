9c4eeb543b75601a9342346c1e173c26b8943e85
Rajas Ilamkar <113300283+RajasIlamkar@users.noreply.github.com>
2025-07-19 17:21:19 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -20,7 +20,7 @@ const Signup = () => {
     setError('');
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/signup', form);
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/signup`, form);
       localStorage.setItem('token', res.data.token);
       localStorage.removeItem('sessionId');
       navigate('/');
